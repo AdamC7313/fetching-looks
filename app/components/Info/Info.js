9d5b77a4f7d764dc232af1cc23cdf79d1ed97ab5
AdamC7313 <AdamC7313@gmail.com>
2024-04-03 19:40:50 -0400

    Add info block

--- app/components/Info/Info.js
+++ app/components/Info/Info.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
+import './Info.css';
 
 const Info = () => {
   return (
-    <div>Info</div>
+    <div className='info-container'>
+      <h1 className='title'>Welcome to Fetching Looks Mobile Grooming</h1>
+      <p className='info-text'> At Fetching Looks we serve Brandon and the Surrounding cities. We are open daily from Tuesday to Saturday. Please use the button below to snag your spot!</p>
+      <button className='appt-request-btn'>Request an Appointment</button>
+    </div>
   )
 }
 
-export default Info
+export default Info;
--- app/page.js
+++ app/page.js
@@ -1,12 +1,17 @@
 import styles from "./page.module.css";
 import HomePage from './components/HomePage/HomePage';
 import NavBar from "./components/NavBar/NavBar";
+import Info from "./components/Info/Info";
+import AboutUs from "./components/AboutUs/AboutUs";
+import Quotes from "./components/Quotes/Quotes";
+import Gallery from "./components/Gallery/Gallery";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <NavBar />
       <HomePage />
+      <Info />
     </main>
   );
 }
